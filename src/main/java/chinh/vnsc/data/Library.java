00160de9946e1adf75113d5d7fa98ab5c71eac94
chinh1995 <chinhvm.uet.1995@gmail.com>
2016-06-16 15:16:48 +0700

    First Commit
    Upload Maven Depencies, First project, Demo

---
+++ src/main/java/chinh/vnsc/data/Library.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chinh.vnsc.data;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import javax.xml.bind.JAXBException;
+import org.apache.sis.storage.DataStoreException;
+import org.apache.sis.storage.geotiff.LandsatReader;
+import org.apache.sis.xml.XML;
+import org.opengis.metadata.Metadata;
+import org.opengis.util.FactoryException;
+
+/**
+ *
+ * @author chinhvm
+ */
+public class Library {
+    private ArrayList<Metadata> data = new ArrayList<Metadata>();
+    
+    public Library() throws IOException, DataStoreException, JAXBException, ParseException, FactoryException {
+        Metadata md1;
+        Metadata md2;
+        LandsatReader reader;
+        try (BufferedReader in = new BufferedReader(new FileReader("/home/chinhvm/Documents/LC81230522014071LGN00_MTL.txt"))) {
+            reader = new LandsatReader(in);
+            md1 = reader.read();
+            data.add(md1);
+        }
+        md2 = (Metadata) XML.unmarshal(new File("/home/chinhvm/Documents/G184220810-LAADS.iso19115"));
+        data.add(md2);
+    }
+    
+    public Metadata getMetadata(int id){
+        return data.get(id);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Jun 15, 2016, 9:22:00 PM
+    Author     : chinhvm
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <a href="enter">Click here to continue</a>
+    </body>
+</html>
